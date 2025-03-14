ee2c382c1caadc0e3cc03924f3f9894b9f19fe68
Denilson <2201010141@undc.edu.pe>
2025-03-14 01:43:34 -0500

    xd

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
-import axios from "axios"
+import axios from "axios";
 
 const instance = axios.create({
-    baseURL : import.meta.env.VITE_API_URL, 
-    withCredentials: true 
+  baseURL: import.meta.env.VITE_API_URL, // Backend en Render o local
+  withCredentials: true, // Esto es lo importante para que la cookie viaje
 });
 
 export default instance;
